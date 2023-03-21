da3b158f8cd0fb8c530b7019477a885b6b699869
Reid Yonkers <81446209+chaiai@users.noreply.github.com>
2023-03-21 17:55:34 -0400

    Update 2_buildTools.sh

--- nano/2_buildTools.sh
+++ nano/2_buildTools.sh
@@ -8,12 +8,13 @@ sudo apt update
 sudo apt -y install git nano ssh openssh-server python3-pip
 sudo -H pip3 install -U jetson-stats
 
-sudo apt -y remove cmake
-sudo chown $USER:$USER /opt
-wget https://github.com/Kitware/CMake/releases/download/v3.26.0/cmake-3.26.0.tar.gz
-tar -xf cmake-3.26.0 /opt/
-cd /opt/cmake-3.26.0
-./configure
-sudo make install
-sudo ldconfig
-cmake --version
+#sudo apt -y remove cmake
+#sudo chown $USER:$USER /opt
+#wget https://github.com/Kitware/CMake/releases/download/v3.26.0/cmake-3.26.0.tar.gz
+#tar -xf cmake-3.26.0 /opt/
+#cd /opt/cmake-3.26.0
+#./configure
+#sudo make install
+#sudo ldconfig
+#cmake --version
+
